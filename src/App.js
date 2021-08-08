71b24fc100e2d5b9407efeff3059fa8a10b7b9a1
labdhijain <labdhijain753@gmail.com>
2021-08-08 15:04:43 -0500

    add gh-pages

--- src/App.js
+++ src/App.js
@@ -2,41 +2,44 @@ import logo from './style/img/logo.svg';
 import './App.css';
 import MentorPage from './components/mentorpage.js'
 import img from "./style/img/background.jpg"
-
+import Navbar from 'react-bootstrap/Navbar'
+import { Nav } from 'react-bootstrap';
+import { NavDropdown } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App" style={{backgroundImage:`url(${img})`, backgroundPosition: 'center',
     backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',}}>
+    backgroundRepeat: 'no-repeat'}}>
 
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" style={{backgroundColor:'#002244'}}>
+  <Container>
+   <img
+        src={logo}
+        width="100"
+        height="100"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+        style={{marginRight:'20px'}}
+      />
+  <Navbar.Brand href="#home"> Mentors </Navbar.Brand>
+  
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    
+   
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
 
-<nav className="navbar navbar-expand-lg" style={{backgroundColor:'#000044'}}>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-    <img src={logo} style={{margin:'20px'}}></img>
-    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-  </div>
-</nav>
 
       
-      <div>
-
-        <MentorPage></MentorPage>
-
+      <div  style={{margin: '1%' }} >
+        <MentorPage> </MentorPage>
       </div>
+
+
     </div>
   );
 }
--- src/components/mentor.js
+++ src/components/mentor.js
@@ -6,10 +6,12 @@ export default function Mentor(props) {
   return (
     <div className='mentor-block'>
      
-    
-      <div className='mentor-image' style= {{ backgroundImage: `url(${props.profile})`}} ></div> <br/>
-      <p className='mentor-name'>{props.firstName} {props.lastName}</p> <br/>
-      <p>{props.description}</p>
+    <div>
+    <div className='mentor-image' style= {{ backgroundImage: `url(${props.profile})`}} ></div> <br/>
+      <p className='mentor-name'>{props.firstName} {props.lastName}</p> 
+    </div>
+  
+      <p className='mentor-description'>{props.description}</p>
     </div>
   );
 }
